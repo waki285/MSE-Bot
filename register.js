9e672a4d221d8edd77b296cbef43fd83b8973fee
waki285 <suzuneu@suzuneu.com>
2021-12-01 12:42:47 +0000

    ✨ feat(SlashCommand): add discord.js builders

---
+++ register.js
@@ -0,0 +1,17 @@
+const { Client, ClientApplication } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+const command = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("BotのPing値を計測します。");
+
+(async () => {
+  const client = new Client({ intents: 0 });
+  client.token = process.env.BOT_TOKEN;
+  client.application = new ClientApplication(client, {});
+  await client.application.fetch();
+
+  await client.application.commands.create(command, process.argv[2]);
+
+  console.log("Registration Success!")
+})()
