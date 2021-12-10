cd2ed444bd9a18b74144f3084da345c39c209a4d
waki285 <suzuneu@suzuneu.com>
2021-12-10 10:33:00 +0000

    ✨ feat: add sayonara msg

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,18 @@
+const { GuildMember } = require("discord.js")
+
+module.exports = {
+  name: "guildMemberRemove",
+  /**
+   * 
+   * @param {GuildMember} member
+   */
+  async execute(member) {
+    member.client.channels.cache.get("798922824349646938").send({
+      content: `${member.user.toString()}**さんが退出しました...**\n\`\`\`diff\n- 現在のサーバー人数: ${member.guild.memberCount}になりました\n\`\`\``,
+      embeds: [{
+        color: "RED",
+        description: `> ${member.user.username}さん、また来てくれると幸いです!!☘`
+      }]
+    })
+  }
+}
